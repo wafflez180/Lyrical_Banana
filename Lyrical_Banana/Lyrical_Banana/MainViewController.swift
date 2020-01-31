b22bc7422ec229b32b36cf5b595920de6bb16dbc
Arthur De Araujo <wafflez180@yahoo.com>
2020-01-31 18:33:05 -0500

    Add MainVC to VideoEditorVC Transition

--- Lyrical_Banana/Lyrical_Banana/MainViewController.swift
+++ Lyrical_Banana/Lyrical_Banana/MainViewController.swift
@@ -7,17 +7,38 @@
 //
 
 import UIKit
+import PMSuperButton
 
 class MainViewController: UIViewController {
-
+    
+    @IBOutlet var videoListContainerView: UIView!
+    
     @IBOutlet var bananaImageView: UIImageView!
     @IBOutlet var noVideosCreatedLabel: UILabel!
-    
+    @IBOutlet var createNewVideoButton: PMSuperButton!
+
+    // MARK: - UIViewController
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
-
     
+    // MARK: - MainViewController
+
+
+    // MARK: - Actions
+    
+    @IBAction func pressedCreateNewVideo(_ sender: Any) {
+        UIView.animate(withDuration: 0.2, delay: 0.0, animations: {
+            self.createNewVideoButton.alpha = 0.0
+        })
+        UIView.animate(withDuration: 0.2, delay: 0.2, animations: {
+            self.noVideosCreatedLabel.alpha = 0.0
+            self.bananaImageView.alpha = 0.0
+        }) { completed in
+            self.videoListContainerView.isHidden = true
+        }
+    }
 }
 
