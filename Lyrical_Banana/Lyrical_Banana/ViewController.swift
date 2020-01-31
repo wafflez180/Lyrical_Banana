3a8b4fe470996c61f9d9deb9838e5c8a3035e6c7
Arthur De Araujo <wafflez180@yahoo.com>
2020-01-31 17:24:52 -0500

    Created XCode project

---
+++ Lyrical_Banana/Lyrical_Banana/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lyrical_Banana
+//
+//  Created by Arthur De Araujo on 1/31/20.
+//  Copyright © 2020 Arthur De Araujo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
